02ff3c550b17e5231063c10b6ac9b04cebedd2e3
Sri Balaji A <sribalaji.anbarasu@outlook.com>
2023-08-19 23:39:23 +0530

    Added more testcases for header

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,10 +1,61 @@
-import Header from './header';
-import '@testing-library/jest-dom';
-import { render as renderWithProvider, screen } from '../../test-utils';
+import Header from "./header";
+import "@testing-library/jest-dom";
+import {
+  render as renderWithProvider,
+  screen,
+  fireEvent,
+} from "../../test-utils";
 
-describe('Simple working test', () => {
-  it('the title is visible', () => {
-    renderWithProvider(<Header />); 
+describe("Header Component", () => {
+  it('displays the title "Crypto Pulse"', () => {
+    renderWithProvider(<Header />);
     expect(screen.getByText(/Crypto Pulse/i)).toBeInTheDocument();
+  });
+
+  it("triggers search with valid input", async () => {
+    // Render the component
+    renderWithProvider(<Header />);
+
+    // Simulate user input and submit
+    const searchInput = screen.getByPlaceholderText("Search currency...");
+    const searchButton = screen.getByText("Search");
+
+    fireEvent.change(searchInput, { target: { value: "1ECO-BTC" } });
+    fireEvent.click(searchButton);
+  });
+
+  it("displays validation error with invalid input", async () => {
+    renderWithProvider(<Header />);
+
+    const searchInput = screen.getByPlaceholderText("Search currency...");
+    const searchButton = screen.getByText("Search");
+
+    fireEvent.change(searchInput, { target: { value: "inv@lid" } });
+    fireEvent.click(searchButton);
+
+    // Assert that the validation error message is displayed
+    const errorText = await screen.findByText("Use Alphanumeric & Hyphen Only");
+    expect(errorText).toBeInTheDocument();
+
+    // Assert that the button is disabled
+    expect(searchButton).toBeDisabled();
+  });
+  it("displays minimum length validation error", async () => {
+    renderWithProvider(<Header />);
+
+    const searchInput = screen.getByPlaceholderText("Search currency...");
+    const searchButton = screen.getByText("Search");
+
+    fireEvent.change(searchInput, { target: { value: "abc" } });
+    fireEvent.click(searchButton);
+
+    // Assert that the validation error message is displayed
+    const errorText = await screen.findByText(
+      "Minimum 6 characters is required"
+    );
+    expect(errorText).toBeInTheDocument();
+
+    // Assert that the button is disabled
+    expect(searchButton).toBeDisabled();
   });
 });
