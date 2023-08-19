fe590335e671348068ef21c661c10eff218fbdb6
Sri Balaji A <sribalaji.anbarasu@outlook.com>
2023-08-20 02:52:40 +0530

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./setupTests"
-  }
+  plugins: [react()]
 })
