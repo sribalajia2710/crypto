032e63a9b9edaefa61172c4c77c9bde297aca846
Sri Balaji A <sribalaji.anbarasu@outlook.com>
2023-08-20 16:29:13 +0530

    jest config file

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-export default {
-    preset: 'ts-jest',
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{js,jsx}'],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-    globals: { 'ts-jest': { diagnostics: false } },
-}
